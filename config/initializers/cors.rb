956ae75d30643bfa069b7763e4716b27b8532010
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-03-13 21:31:04 -0600

    Restore cors configs

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,7 +3,7 @@
 # Be sure to replace 'http://localhost:5173' with the actual origin of your React app.
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://book-a-concert.onrender.com' 
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    origins '*' 
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
   end
 end
