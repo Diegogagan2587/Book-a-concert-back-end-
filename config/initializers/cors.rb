20071788b302fd81c8f1567c752362a322263227
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-03-13 21:57:31 -0600

    fix/cors: add localhost and production url as origins, include had as method, add max_age

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,7 +3,12 @@
 # Be sure to replace 'http://localhost:5173' with the actual origin of your React app.
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*' 
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
+    origins "http://localhost:5173",
+            "https://book-a-concert.onrender.com"
+    resource '*', 
+      headers: :any, 
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true,
+      max_age: 86400
   end
 end
